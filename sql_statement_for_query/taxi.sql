b4fe2308b243d6b6ec2c06af69e60117b1b01c99
personryan <mageryan1@gmail.com>
2022-09-19 22:52:22 +0800

    MRT & Taxi
    SQL Statement: to find MRT & Taxi based on bus service selected
    
    Added API:
    getMRTStationName
    getMRTStationNameFromServiceNo
    getTaxiStandFromServiceNo

--- sql_statement_for_query/taxi.sql
+++ sql_statement_for_query/taxi.sql
@@ -5,4 +5,15 @@ Example:WHERE NAME LIKE '%Bedok%';
 SELECT TaxiCode, IF(Bfa = 1, 'YES', 'No') as "Barrier Free", Type, Name, StnCode as "Nearest MRT"
 FROM Taxi_Stand WHERE Name LIKE '%%';
 
+/*
+Find Taxi Stand for the MRT Station selected by user
+Example:WHERE ServiceNo = '98';
+*/
+SELECT Name
+FROM Taxi_Stand taxi
+WHERE taxi.StnCode IN (
+SELECT DISTINCT mrt.StnCode
+FROM MRT_Station mrt
+LEFT JOIN bus_route br ON mrt.busStopCode = br.busStopCode
+WHERE ServiceNo = '98');
 
