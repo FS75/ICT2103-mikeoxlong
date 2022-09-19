e29106939acd4645d882247b35764380d50785f2
brucewzj99 <bruceloveapple@gmail.com>
2022-09-19 11:30:28 +0800

    update script with description

--- setup.sh
+++ setup.sh
@@ -1,3 +1,8 @@
+echo "Going into frontend/2103-project directory"
 cd frontend/2103-project
+echo "Running npm install"
 npm install
+echo "Running npm update"
 npm update
+echo "End of script, closing in 10"
+sleep 10
--- start.sh
+++ start.sh
@@ -1,2 +1,7 @@
+echo "Going into frontend/2103-project directory"
 cd frontend/2103-project
-npm run dev
+echo "npm run dev"
+npm run dev &
+(sleep 10
+echo "Opening browser"
+start http://localhost:8080/ & open http://localhost:8080/)
