4c414e1bcfc0af273ad85689e1ae0e31fc5eb506
brucewzj99 <bruceloveapple@gmail.com>
2022-09-20 10:27:57 +0800

    update script

--- start.sh
+++ start.sh
@@ -1,7 +1,6 @@
 echo "Going into frontend/2103-project directory"
 cd frontend/2103-project
 echo "npm run dev"
-npm run dev &
 (sleep 10
 echo "Opening browser"
-start http://localhost:8080/ & open http://localhost:8080/)
+start http://localhost:8080/ & open http://localhost:8080/) & npm run dev
