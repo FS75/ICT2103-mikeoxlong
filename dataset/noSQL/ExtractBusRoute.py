2f72e44e55648d41d67610b2e00ddf9079592693
personryan <mageryan1@gmail.com>
2022-11-10 18:33:39 +0800

    Python file for extracting data, update of directory

---
+++ dataset/noSQL/ExtractBusRoute.py
@@ -0,0 +1,17 @@
+import json
+
+f = open('bus_routes copy.json')
+# returns JSON object as
+# a dictionary
+data = json.load(f)
+list1 = []
+# Iterating through the json
+# list
+print(type(data['value']))
+for i in data['value']:
+    if i['ServiceNo'] == '116' and i['Direction'] == 1:
+        list1.append(i)
+
+with open('myfile.txt', 'w') as convert_file:
+    convert_file.write(json.dumps(list1))
+f.close()
