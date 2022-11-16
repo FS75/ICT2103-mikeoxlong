f0945c9d123a0aba591e0fb7042ef1f0fb643515
personryan <mageryan1@gmail.com>
2022-11-17 00:11:40 +0800

    Converted SQL query for BUS to noSQL

--- dataset/noSQL/ExtractMRTStation.py
+++ dataset/noSQL/ExtractMRTStation.py
@@ -1,9 +1,10 @@
 import pandas as pd
 import json
-df = pd.read_excel('Train_station_codes.xls')
-df = df.drop(columns=['mrt_station_chinese', 'mrt_line_chinese'])
-df = df.rename(columns={"mrt_station_english": "MRT_station", "mrt_line_english": "MRT_line"})
-df['busStop_code'] = "a"
+# df = pd.read_excel('mrt_station.xls')
+df = pd.read_excel('taxi_stand.xls')
+#df = df.drop(columns=['mrt_station_chinese', 'mrt_line_chinese'])
+#df = df.rename(columns={"mrt_station_english": "MRT_station", "mrt_line_english": "MRT_line"})
+#df['busStop_code'] = "a"
 df.to_excel("output.xlsx")
 
 # displaying the DataFrame
